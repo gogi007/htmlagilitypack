f107e994fd1674c26ef6570d5ae30b1d0cd1f2b1
gogi007 <gogitibi89@gmail.com>
2017-02-02 00:11:43 +0100

    Add project files.

---
+++ htmlagilitypack/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace htmlagilitypack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new HtmlDocument();
+            doc.Load
+        }
+    }
+}
